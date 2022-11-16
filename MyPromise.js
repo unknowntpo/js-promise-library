dd822cb7d7e169329a1f7d336520cd9cd6d48bac
Chen-Chien Chang (張辰謙) <ds_chen-chien.chang@mediatek.com>
2022-11-16 08:23:03 +0800

    add partially ok code: until 8:25
    https://youtu.be/1l4wHWQCCIc?t=505

--- MyPromise.js
+++ MyPromise.js
@@ -1,5 +1,47 @@
+const STATE = {
+    FULFILLED: 'fulfilled',
+    REJECTED: 'rejected',
+    PENDING: 'pending'
+}
 class myPromise {
+    #thenCbs = []
+    #state = PENDING
 
+    constructor(cb) {
+        try {
+            cb(this.#onSuccess, this.#onFail)
+        } catch (e) {
+            this.#onFail(e)
+        }
+    }
+
+    #onSuccess(value) {
+        // resolve should be handled only once 
+        if (this.#state !== STATE.PENDING) return
+        this.#value = value
+        this.#state = STATE.FULFILLED
+    }
+
+    #onFail(value) {
+        // reject should be handled only once 
+        if (this.#state !== STATE.PENDING) return
+        this.#value = value
+        this.#state = STATE.REJECTED
+    }
+
+    then(cb) {
+        this.#thenCbs.push(cb)
+    }
 }
 
 module.exports = MyPromise
+
+// Example usage
+
+const p = new MyPromise((resolve, reject) => {
+    resolve('OK')
+    reject('FAIL')
+}).then()
+
+p.then()
+p.then()
